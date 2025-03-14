7e02fbf0c146bff5e16bef71dd7cf93ab241e8d4
Farhad <Bigonahibit@gmail.com>
2025-03-14 16:35:29 +0330

    remove unnecessary CORS headers from axios instance configuration

--- lib/api.ts
+++ lib/api.ts
@@ -15,9 +15,6 @@ const axiosInstance = axios.create({
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-		"Access-Control-Allow-Credentials": "true",
 	},
 });
 
